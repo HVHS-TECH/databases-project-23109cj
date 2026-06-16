66bdb8fd24500418e35ffbe8f42b581be3bca828
23109cj <23109cj@hvhs.school.nz>
2026-06-16 12:29:41 +1200

    writing problems

--- fb_io.js
+++ fb_io.js
@@ -8,28 +8,28 @@
  **************************************************************/
 let canLogIn = true;
 
-function fb_login(){
+function fb_login() {
     let userName = document.getElementById('userName').value.trim();
     let age = document.getElementById('age').value.trim();
 
-    if(userName.includes('<') || userName.includes('>')){
+    if (userName.includes('<') || userName.includes('>')) {
         alert('You cannot have a username that includes < or >')
         return;
-    } else if(age<13){
+    } else if (age < 13) {
         alert('You have to be at least 13 to use this site');
         return;
     }
-   console.log(userName)
-   console.log(age)
-    if(age != '' && userName != ''){
+    console.log(userName)
+    console.log(age)
+    if (age != '' && userName != '') {
         fb_authenticate(userName, age)
-    } else{
+    } else {
         alert('Please ensure you have filled out all input fields')
         return;
     }
 }
 
-function fb_authenticate(_userName, _age){
+async function fb_authenticate(_userName, _age) {
     // authenticate with Google
     if (canLogIn) {
         var authentication = firebase.auth().onAuthStateChanged((user) => {
@@ -50,8 +50,8 @@ function fb_authenticate(_userName, _age){
                 // User is signed in, see docs for a list of available properties
                 // https://firebase.google.com/docs/reference/js/v8/firebase.User
                 let uid = user.uid;
-                sessionStorage.setItem('uid',uid)
-                sessionStorage.setItem('userName',_userName)
+                sessionStorage.setItem('uid', uid)
+                sessionStorage.setItem('userName', _userName)
                 let name = user.displayName;
                 let photoURL = user.photoURL;
                 let email = user.email;
@@ -60,7 +60,7 @@ function fb_authenticate(_userName, _age){
                 authentication();
 
                 fb_checkBan(user.uid)
-
+                console.log('starting write')
                 firebase.database().ref('/userInfo/' + uid + '/userName').set(_userName)
                 firebase.database().ref('/userInfo/' + uid + '/uid').set(uid);
                 firebase.database().ref('/userInfo/' + uid + '/name').set(name);
@@ -69,7 +69,10 @@ function fb_authenticate(_userName, _age){
                 firebase.database().ref('/userInfo/' + uid + '/phoneNumber').set(phoneNumber);
                 firebase.database().ref('/userInfo/' + uid + '/age').set(_age);
 
-                window.location.href="gameSelection.html"
+                //need to change to updatiung inner html, rather than changing pages
+                document.getElementById('body').innerHTML =``
+
+                //window.location.href="gameSelection.html"
 
             } else {
                 console.log('not logged in')
@@ -81,28 +84,28 @@ function fb_authenticate(_userName, _age){
     }
 }
 
-function fb_error(){
+function fb_error() {
     // Don't forget your error handling!
 }
 
-async function fb_writeHighscore(_gameName, _score,){
+async function fb_writeHighscore(_gameName, _score,) {
     let uid = sessionStorage.getItem('uid')
     console.log(uid)
-    let snapshot = await firebase.database().ref('/userInfo/'+uid).once('value');
+    let snapshot = await firebase.database().ref('/userInfo/' + uid).once('value');
     let snapshotValue = snapshot.val();
     let userName = snapshotValue.userName;
-    let scoreSnapshot = await firebase.database().ref('/'+_gameName+'Highscore/'+uid+'/score').once('value');
+    let scoreSnapshot = await firebase.database().ref('/' + _gameName + 'Highscore/' + uid + '/score').once('value');
     let currentScore;
-    if(scoreSnapshot.exists()){
+    if (scoreSnapshot.exists()) {
         let scoreSnapshotValue = scoreSnapshot.val();
         currentScore = scoreSnapshotValue.score;
-    }else{
-         currentScore = 0;
+    } else {
+        currentScore = 0;
     }
-    if(_score >= currentScore || currentScore == null){
+    if (_score >= currentScore || currentScore == null) {
         console.log('writing')
-        firebase.database().ref('/'+_gameName+'Highscore/'+uid+'/userName').set(userName);
-        firebase.database().ref('/'+_gameName+'Highscore/'+uid+'/score').set(_score);
+        firebase.database().ref('/' + _gameName + 'Highscore/' + uid + '/userName').set(userName);
+        firebase.database().ref('/' + _gameName + 'Highscore/' + uid + '/score').set(_score);
     }
 }
 
@@ -112,38 +115,38 @@ async function fb_checkBan(_userUID) {
     if (banned != null) {
         return true;
         window.location.href = 'https://en.wikipedia.org/wiki/Hacker';
-    } else{
+    } else {
         return false;
     }
 }
 
 async function fb_readHighscore(_gameName) {
     console.log('fb_readHighscore()')
-    let snapshot = await firebase.database().ref('/'+_gameName+'Highscore').once('value');
+    let snapshot = await firebase.database().ref('/' + _gameName + 'Highscore').once('value');
     let highscoreSnapshot = snapshot.val();
     let highscoreTable = [];
     let highscoreKeys = Object.values(highscoreSnapshot)
 
-    for(i=0; i<highscoreKeys.length; i++){
+    for (i = 0; i < highscoreKeys.length; i++) {
         let userKey = highscoreKeys[i].userName;
         let scoreKey = highscoreKeys[i].score;
-        let userDetails = {'userName': userKey,  'score': scoreKey}
-        
+        let userDetails = { 'userName': userKey, 'score': scoreKey }
+
         highscoreTable.push(userDetails)
     }
-    highscoreTable.sort((a,b) => b.score - a.score)
+    highscoreTable.sort((a, b) => b.score - a.score)
     console.log(highscoreTable)
-    
+
     let output = document.getElementById('output');
     output.innerHTML = '';
 
     let uid = sessionStorage.getItem('uid')
     let userName = sessionStorage.getItem('userName')
-    let currentUserSnapshot = await firebase.database().ref('/'+_gameName+'Highscore/'+uid+'/score').once('value');
+    let currentUserSnapshot = await firebase.database().ref('/' + _gameName + 'Highscore/' + uid + '/score').once('value');
     let currentUserHighscore = currentUserSnapshot.val();
-    output.innerText = 'Your Score: '+currentUserHighscore+'\n\n The Top 5 Highscores are:\n';
+    output.innerText = 'Your Score: ' + currentUserHighscore + '\n\n The Top 5 Highscores are:\n';
 
-    for(i=0; i<4 && i<highscoreTable.length; i++){
-        output.innerText += `\n`+ highscoreTable[i].userName +`: `+highscoreTable[i].score;
+    for (i = 0; i < 4 && i < highscoreTable.length; i++) {
+        output.innerText += `\n` + highscoreTable[i].userName + `: ` + highscoreTable[i].score;
     }
 }
